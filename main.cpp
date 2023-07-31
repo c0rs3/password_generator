f0f42cc14af966abbbdb6c952aa7f24fff9351d8
Tuna Arıyürek <ariyurektuna@gmail.com>
2023-07-31 20:07:27 +0300

    few tweaks

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 #include "password_generator.hpp"
 
 using namespace generator;
-
-std::ostream &operator<<(std::ostream &stream, std::vector<uint32_t> char_stats) {
+// operator overloading for displaying char stats
+std::ostream &operator<<(std::ostream &stream, std::vector<uint32_t> char_stats)
+{
     stream << "Upper Characters: " << char_stats[0] << std::endl;
     stream << "Lower Characters: " << char_stats[1] << std::endl;
     stream << "Digits: " << char_stats[2] << std::endl;
@@ -11,22 +12,21 @@ std::ostream &operator<<(std::ostream &stream, std::vector<uint32_t> char_stats)
     return stream;
 }
 
-int main() {
+int main()
+{
+
     std::string sample_string = "Aa1!";
     std::string sample_password1 = generate_password(18); // generates a 18-char-password
-    std::string sample_password2 = "QDMM,MNi0ISP7dYi";    // sample password for demonstrating remove_duplicate() & any_dupes() functions
+    std::string sample_password2 = "QDMM,MNi0ISP7dYi";    // sample password for demonstrating remove_adjacent_duplicate() & has_adjacent_duplicates() functions
 
     std::cout << "Random integer: " << random_int(0, 10) << std::endl;
     std::cout << "Maximum entropy of a 18 character password: " << max_entropy(18) << std::endl
-              << std::endl;
-    // returns the maximum possible entropy bits of a 18 char password
-    std::cout << "Char type of 'A': " << chartypeof('A') << ":" << std::endl;
-
+              << std::endl; // returns the maximum possible entropy bits of a 18 char password
+    std::cout << "Char type of 'A': " << get_char_type('A') << ":" << std::endl;
     /* upper case -> 0
      *  lower case -> 1
      *  digit -> 2
      *  symbols -> 3 */
-
     std::cout << "Char stats of " << sample_string << "\n"
               << char_stats(sample_string) << std::endl
               << std::endl;
@@ -36,8 +36,10 @@ int main() {
               << std::endl;
 
     std::cout << "Example password with duplicates: " << sample_password2 << std::endl;
-    std::cout << "Any duplicates? " << any_dupes(sample_password2) << std::endl;
-    remove_duplicates(sample_password2);
+    std::cout << "Any duplicates? " << std::boolalpha << has_adjacent_duplicates(sample_password2) << std::endl;
+    remove_adjacent_duplicates(sample_password2);
     std::cout << "After removing duplicates: " << sample_password2 << std::endl;
-    std::cout << "Any duplicates? " << any_dupes(sample_password2) << std::endl;
+    std::cout << "Any duplicates? " << std::boolalpha << has_adjacent_duplicates(sample_password2) << std::endl;
+
+    return 0;
 }
