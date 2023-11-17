3d2349ed8042863df19cb0058c0c5261899e8d82
Tuna Arıyürek <ariyurektuna@gmail.com>
2023-11-17 21:22:27 +0300

    remaining tests

--- tests/benchmark.cpp
+++ tests/benchmark.cpp
@@ -7,7 +7,7 @@ void benchmark_strength(const size_t& size = 10){
     std::string generated_password = generator::generate_password(18);
     for (size_t i = 0; i < size; i++) {
         if (entropy_bits(generated_password) == max_entropy(18)) {
-            std::cout << "Maximum possible strength password produced" << std::endl;
+            std::cout << "Maximum possible strength password produced|" << size-i <<" remaining..."<<std::endl;
         } else {
             std::cout << "Failed to produce maximum possible strength " << std::endl;
         }
@@ -15,6 +15,6 @@ void benchmark_strength(const size_t& size = 10){
 }
 
 int main() { 
-    benchmark_strength(1000);   
+    benchmark_strength(1000);
     return 0;
 }
